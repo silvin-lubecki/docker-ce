tiborvass/docker

Clean some integration-cli/fixtures package/files
- Move go package used by both `integration-cli` and `integration` to
  `internal/test/fixtures`.
- Remove fixtures that are not used anymore (moved to `docker/cli` a
  while ago) : deploy, notary, secrets.

Signed-off-by: Vincent Demeester <vincent@sbr.pm>

integration-cli/check_test.go
@@ -19,9 +19,9 @@ import (
 	"github.com/tiborvass/docker/integration-cli/cli/build/fakestorage"
 	"github.com/tiborvass/docker/integration-cli/daemon"
 	"github.com/tiborvass/docker/integration-cli/environment"
-	"github.com/tiborvass/docker/integration-cli/fixtures/plugin"
 	testdaemon "github.com/tiborvass/docker/internal/test/daemon"
 	ienv "github.com/tiborvass/docker/internal/test/environment"
+	"github.com/tiborvass/docker/internal/test/fixtures/plugin"
 	"github.com/tiborvass/docker/internal/test/registry"
 	"github.com/tiborvass/docker/pkg/reexec"
 	"github.com/go-check/check"

integration-cli/docker_api_swarm_service_test.go
@@ -14,8 +14,8 @@ import (
 	"github.com/tiborvass/docker/api/types/swarm/runtime"
 	"github.com/tiborvass/docker/integration-cli/checker"
 	"github.com/tiborvass/docker/integration-cli/daemon"
-	"github.com/tiborvass/docker/integration-cli/fixtures/plugin"
 	testdaemon "github.com/tiborvass/docker/internal/test/daemon"
+	"github.com/tiborvass/docker/internal/test/fixtures/plugin"
 	"github.com/tiborvass/docker/internal/test/registry"
 	"github.com/go-check/check"
 	"golang.org/x/net/context"

integration-cli/docker_cli_plugins_test.go
@@ -14,7 +14,7 @@ import (
 	"github.com/tiborvass/docker/integration-cli/checker"
 	"github.com/tiborvass/docker/integration-cli/cli"
 	"github.com/tiborvass/docker/integration-cli/daemon"
-	"github.com/tiborvass/docker/integration-cli/fixtures/plugin"
+	"github.com/tiborvass/docker/internal/test/fixtures/plugin"
 	"github.com/go-check/check"
 	"golang.org/x/net/context"
 )

integration-cli/fixtures_linux_daemon_test.go
@@ -11,7 +11,7 @@ import (
 	"sync"
 
 	"github.com/tiborvass/docker/integration-cli/checker"
-	"github.com/tiborvass/docker/integration-cli/fixtures/load"
+	"github.com/tiborvass/docker/internal/test/fixtures/load"
 	"github.com/go-check/check"
 )
 

integration/plugin/logging/helpers_test.go
@@ -9,7 +9,7 @@ import (
 	"time"
 
 	"github.com/tiborvass/docker/api/types"
-	"github.com/tiborvass/docker/integration-cli/fixtures/plugin"
+	"github.com/tiborvass/docker/internal/test/fixtures/plugin"
 	"github.com/tiborvass/docker/pkg/locker"
 	"github.com/pkg/errors"
 )

integration/plugin/volumes/mounts_test.go
@@ -7,8 +7,8 @@ import (
 	"testing"
 
 	"github.com/tiborvass/docker/api/types"
-	"github.com/tiborvass/docker/integration-cli/fixtures/plugin"
 	"github.com/tiborvass/docker/internal/test/daemon"
+	"github.com/tiborvass/docker/internal/test/fixtures/plugin"
 	"github.com/gotestyourself/gotestyourself/assert"
 )
 

internal/test/fixtures/load/frozen.go
@@ -1,4 +1,4 @@
-package load // import "github.com/tiborvass/docker/integration-cli/fixtures/load"
+package load // import "github.com/tiborvass/docker/internal/test/fixtures/load"
 
 import (
 	"bufio"

internal/test/fixtures/plugin/plugin.go
@@ -1,4 +1,4 @@
-package plugin // import "github.com/tiborvass/docker/integration-cli/fixtures/plugin"
+package plugin // import "github.com/tiborvass/docker/internal/test/fixtures/plugin"
 
 import (
 	"encoding/json"
@@ -206,8 +206,9 @@ func ensureBasicPluginBin() (string, error) {
 		return "", err
 	}
 	installPath := filepath.Join(os.Getenv("GOPATH"), "bin", name)
-	cmd := exec.Command(goBin, "build", "-o", installPath, "./"+filepath.Join("fixtures", "plugin", "basic"))
-	cmd.Env = append(cmd.Env, "CGO_ENABLED=0")
+	sourcePath := filepath.Join("github.com", "docker", "docker", "internal", "test", "fixtures", "plugin", "basic")
+	cmd := exec.Command(goBin, "build", "-o", installPath, sourcePath)
+	cmd.Env = append(cmd.Env, "GOPATH="+os.Getenv("GOPATH"), "CGO_ENABLED=0")
 	if out, err := cmd.CombinedOutput(); err != nil {
 		return "", errors.Wrapf(err, "error building basic plugin bin: %s", string(out))
 	}

silvin-lubecki/docker-ce

(cherry picked from commit 5f56503f583f21d655394f755f71849381bd58c7)
Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

integration-cli/check_test.go
@@ -19,9 +19,9 @@ import (
 	"github.com/docker/docker/integration-cli/cli/build/fakestorage"
 	"github.com/docker/docker/integration-cli/daemon"
 	"github.com/docker/docker/integration-cli/environment"
-	"github.com/docker/docker/integration-cli/fixtures/plugin"
 	testdaemon "github.com/docker/docker/internal/test/daemon"
 	ienv "github.com/docker/docker/internal/test/environment"
+	"github.com/docker/docker/internal/test/fixtures/plugin"
 	"github.com/docker/docker/internal/test/registry"
 	"github.com/docker/docker/pkg/reexec"
 	"github.com/go-check/check"

integration-cli/docker_api_swarm_service_test.go
@@ -14,8 +14,8 @@ import (
 	"github.com/docker/docker/api/types/swarm/runtime"
 	"github.com/docker/docker/integration-cli/checker"
 	"github.com/docker/docker/integration-cli/daemon"
-	"github.com/docker/docker/integration-cli/fixtures/plugin"
 	testdaemon "github.com/docker/docker/internal/test/daemon"
+	"github.com/docker/docker/internal/test/fixtures/plugin"
 	"github.com/docker/docker/internal/test/registry"
 	"github.com/go-check/check"
 	"golang.org/x/net/context"

integration-cli/docker_cli_plugins_test.go
@@ -14,7 +14,7 @@ import (
 	"github.com/docker/docker/integration-cli/checker"
 	"github.com/docker/docker/integration-cli/cli"
 	"github.com/docker/docker/integration-cli/daemon"
-	"github.com/docker/docker/integration-cli/fixtures/plugin"
+	"github.com/docker/docker/internal/test/fixtures/plugin"
 	"github.com/go-check/check"
 	"golang.org/x/net/context"
 )

integration-cli/fixtures_linux_daemon_test.go
@@ -11,7 +11,7 @@ import (
 	"sync"
 
 	"github.com/docker/docker/integration-cli/checker"
-	"github.com/docker/docker/integration-cli/fixtures/load"
+	"github.com/docker/docker/internal/test/fixtures/load"
 	"github.com/go-check/check"
 )
 

integration/plugin/logging/helpers_test.go
@@ -9,7 +9,7 @@ import (
 	"time"
 
 	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/integration-cli/fixtures/plugin"
+	"github.com/docker/docker/internal/test/fixtures/plugin"
 	"github.com/docker/docker/pkg/locker"
 	"github.com/pkg/errors"
 )

integration/plugin/volumes/mounts_test.go
@@ -7,8 +7,8 @@ import (
 	"testing"
 
 	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/integration-cli/fixtures/plugin"
 	"github.com/docker/docker/internal/test/daemon"
+	"github.com/docker/docker/internal/test/fixtures/plugin"
 	"github.com/gotestyourself/gotestyourself/assert"
 )
 

internal/test/fixtures/load/frozen.go
@@ -1,4 +1,4 @@
-package load // import "github.com/docker/docker/integration-cli/fixtures/load"
+package load // import "github.com/docker/docker/internal/test/fixtures/load"
 
 import (
 	"bufio"

internal/test/fixtures/plugin/plugin.go
@@ -1,4 +1,4 @@
-package plugin // import "github.com/docker/docker/integration-cli/fixtures/plugin"
+package plugin // import "github.com/docker/docker/internal/test/fixtures/plugin"
 
 import (
 	"encoding/json"
@@ -206,8 +206,9 @@ func ensureBasicPluginBin() (string, error) {
 		return "", err
 	}
 	installPath := filepath.Join(os.Getenv("GOPATH"), "bin", name)
-	cmd := exec.Command(goBin, "build", "-o", installPath, "./"+filepath.Join("fixtures", "plugin", "basic"))
-	cmd.Env = append(cmd.Env, "CGO_ENABLED=0")
+	sourcePath := filepath.Join("github.com", "docker", "docker", "internal", "test", "fixtures", "plugin", "basic")
+	cmd := exec.Command(goBin, "build", "-o", installPath, sourcePath)
+	cmd.Env = append(cmd.Env, "GOPATH="+os.Getenv("GOPATH"), "CGO_ENABLED=0")
 	if out, err := cmd.CombinedOutput(); err != nil {
 		return "", errors.Wrapf(err, "error building basic plugin bin: %s", string(out))
 	}